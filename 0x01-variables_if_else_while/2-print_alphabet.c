17bda4dad52a609d8c71ec23b98828ed008fff6b
hamzaafiricai <mouflih.hamza@gmail.com>
2023-03-17 17:42:46 -0700

    decimal

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+i#include <stdio.h>
+/**
+ * main - print alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+
