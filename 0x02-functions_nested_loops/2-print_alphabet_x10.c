31c96d48a502ef67fd90b4bd6bec1d996576648f
hamzaafiricai <mouflih.hamza@gmail.com>
2023-03-21 15:51:31 -0700

    loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - Prints alphabets 10 time
+ * Return:0
+ */
+void print_alphabet_x10(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		print_alphabet();
+		n++;
+	}
+}
