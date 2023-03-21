a8c66a5a3f5545d3c0e43b089096f72a3ad4bc1e
hamzaafiricai <mouflih.hamza@gmail.com>
2023-03-21 16:27:40 -0700

    loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * _isalpha - X
+ * @c: X
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c <= 65 && c <= 90)
+		return (1);
+	return (0);
+}
