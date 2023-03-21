5eafaae78d33e4193ed74cf1b67d477f706e42f0
hamzaafiricai <mouflih.hamza@gmail.com>
2023-03-21 16:41:34 -0700

    loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,9 +7,14 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
+
+	if (isalpha(c))
+	{
 		return (1);
-	if (c <= 65 && c <= 90)
-		return (1);
-	return (0);
+	}
+	else
+	{
+		return (0);
+	}
+	putchar('\n');
 }
